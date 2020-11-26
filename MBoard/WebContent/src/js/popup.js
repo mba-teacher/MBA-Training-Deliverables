c980f5cfb5b1d4ed4be1d75d9696f01026597348
asano <asano@mba-international.jp>
2020-11-26 17:59:09 +0900

    「3.1.1.1.アカウント作成画面」のjsp、css、jsの編集

--- MBoard/WebContent/src/js/popup.js
+++ MBoard/WebContent/src/js/popup.js
@@ -1,18 +1,6 @@
 /**
- *
+ * モーダルポップアップの表示・非表示
  */
-
-/*function popupCancel() {
-	//var popup = document.getElementsById('cancel');
-	//if(!popup) return;
-
-	//var
-
-	var res = confirm("編集は保持されませんがよろしいですか？");
-	if (res == true) {
-		location.href = "#";
-	}
-}*/
 
 $(function () {
   $('#cancel').click(function(){
@@ -24,3 +12,28 @@ $(function () {
   });
 
 });
+
+function formCheck(){
+    var flag = 0;
+
+    // 入力必須項目が入力されているかチェック
+    if( document . Form1 . Login_ID . value == "" ){ // 名前が未入力の場合
+        flag = 1;
+        document . getElementById( 'inputItem1' ) . style . display = "block"; //表示
+    }else{ // 名前が入力済みの場合
+        document . getElementById( 'inputItem1' ) . style . display = "none"; //非表示
+    }
+    if( document . Form1 . Login_Pass . value == "" ){ // コメントが未入力の場合
+        flag = 1;
+        document . getElementById( 'inputItem1' ) . style . display = "block"; //表示
+    }else{ // コメントが入力済みの場合
+        document . getElementById( 'inputItem1' ) . style . display = "none"; //非表示
+    }
+
+    if( flag ){ // 入力必須項目に未入力があった場合
+        return false; // 送信中止
+    }else{ // 入力必須項目が全て入力済みだった場合
+        return true; // 送信実行
+    }
+
+}
